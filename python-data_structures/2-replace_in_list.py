5127b7963e3798b55492dff64e6fa7f8c56a5f4e
pavithpr89 <pavithpr89@gmail.com>
2026-01-31 08:43:44 +0000

    Did the 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """Replace an element at a given index, or return original list if invalid."""
+    """Replace element at index if valid, else return original list."""
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
