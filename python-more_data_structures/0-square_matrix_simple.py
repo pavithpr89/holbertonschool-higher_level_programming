c35f85a5041946dec3b2d64e165cb237d77467d4
pavithpr89 <pavithpr89@gmail.com>
2026-02-01 10:11:07 +0000

    Did the first

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Return a new matrix with each value squared."""
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for value in row:
+            new_row.append(value * value)
+        new_matrix.append(new_row)
+
+    return new_matrix
