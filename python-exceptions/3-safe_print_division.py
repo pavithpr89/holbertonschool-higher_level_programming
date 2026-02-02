f96a9089b0877cdbeb95f149163c6adffdfd38ea
pavithpr89 <pavithpr89@gmail.com>
2026-02-02 03:22:51 +0000

    Did the 4

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     """Divide 2 integers and print the result safely."""
     try:
         result = a / b
-        return result
     except Exception:
-        return None
+        result = None
     finally:
-        print("Inside result: {}".format(result if 'result' in locals() else None))
+        print("Inside result: {}".format(result))
+    return result
