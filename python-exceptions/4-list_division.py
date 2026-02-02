ec28fc4251111537322a4f4da04015f50182dbf0
pavithpr89 <pavithpr89@gmail.com>
2026-02-02 03:26:04 +0000

    Did the 5

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,4 +2,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     """Divide element by element 2 lists with error handling."""
     result = []
-   
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            result.append(div)
+    return result
